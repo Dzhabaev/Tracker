7fe89559b068e53959ec7ba0a90e7439e6d32b83
Chingiz <dzhabaev@outlook.com>
2024-04-09 23:03:27 +0300

    Добавлены скриншот-тесты

---
+++ TrackerTests/TrackerTests.swift
@@ -0,0 +1,23 @@
+//
+//  TrackerTests.swift
+//  TrackerTests
+//
+//  Created by Chingiz on 09.04.2024.
+//
+
+import XCTest
+import SnapshotTesting
+@testable import Tracker
+
+final class TrackerTests: XCTestCase {
+    
+    func testTrackersViewControllerLightMode() {
+        let vc = TrackersViewController()
+        assertSnapshot(matching: vc, as: .image(traits: .init(userInterfaceStyle: .light)))
+    }
+    
+    func testTrackersViewControllerDarkMode() {
+        let vc = TrackersViewController()
+        assertSnapshot(matching: vc, as: .image(traits: .init(userInterfaceStyle: .dark)))
+    }
+}
